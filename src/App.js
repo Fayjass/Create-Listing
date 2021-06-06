ee9465975189366a6b88fa6d03cd39b1a3a5a0ec
Vera <v-brus@yandex.ru>
2021-06-06 13:20:13 +0500

    Remove todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './App.css'
 import TodoList from './Todo/TodoList.js'
+import Context from './context.js'
+import AddTodo from './Todo/AddTodo'
 
 function App() {
   const [todos, setTodos] = React.useState([
+    // ! now don`t use hook "useState" into import React(?)
     { id: 0, completed: false, title: 'Купить хлеб' },
     { id: 1, completed: false, title: 'Купить молоко' },
     { id: 2, completed: false, title: 'Купить масло' },
@@ -11,21 +14,33 @@ function App() {
   console.log('start', 'todos', todos)
   function toggleTodo(id) {
     console.log('id', id)
+
     setTodos(
-      todos.map((item) => {
-        if (item.id === id) {
-          item.completed = !item.completed
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
         }
         console.log('todos', todos)
-        return item
+        return todo
       })
     )
   }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
   return (
-    <div className='wrapper'>
-      <h1>React titorial</h1>
-      <TodoList todos={todos} onToggle={toggleTodo} />
-    </div>
+    <Context.Provider value={{ removeTodo: removeTodo }}>
+      <div className='wrapper'>
+        <h1>React titorial</h1>
+
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : (
+          <p>the list is closed</p>
+        )}
+      </div>
+    </Context.Provider>
   )
 }
 
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
+import Context from '../context'
+
 const styles = {
   li: {
     display: 'flex',
@@ -16,6 +18,7 @@ const styles = {
 }
 
 function TodoItem({ todo, index, onChange }) {
+  const { removeTodo } = useContext(Context)
   let classes = []
   console.log('todo.completed', todo.completed)
   if (todo.completed) {
@@ -36,7 +39,9 @@ function TodoItem({ todo, index, onChange }) {
         {todo.title}
       </span>
       {/* &times - это крестик на кнопке */}
-      <button className='rm'>&times;</button>
+      <button className='rm' onClick={removeTodo.bind(null, todo.id)}>
+        &times;
+      </button>
     </li>
   )
 }
